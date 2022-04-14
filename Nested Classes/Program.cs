b3706608f6460223b93e31cecd1147bf4dc9d1f7
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-15 00:59:44 +0100

    Create Program.cs

---
+++ Nested Classes/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_2_and_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Faculty fclt = new Faculty();
+			fclt.SetData();
+			Console.ReadKey();
+		}
+	}
+}
