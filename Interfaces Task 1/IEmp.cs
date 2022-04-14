5f50fe1f0bcd29997a7dc8c87771d6539b593dff
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-15 00:37:30 +0100

    Create IEmp.cs

---
+++ Interfaces Task 1/IEmp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab10_1
+{
+	interface IEmp
+	{
+		void Role();
+		void Salary();
+		string Name { get; set; }
+		int Work_Exp { get; set; }
+	}
+}
