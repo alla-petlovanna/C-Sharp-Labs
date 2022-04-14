464625ec4e27a813ac382b300498b11c60e5d308
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-14 23:13:43 +0100

    Update Counting_the_number_of_characters_in_a_row_and_outputting_a_row_to_a_column.cs

--- Counting_the_number_of_characters_in_a_row_and_outputting_a_row_to_a_column.cs
+++ Counting_the_number_of_characters_in_a_row_and_outputting_a_row_to_a_column.cs
@@ -6,6 +6,11 @@ namespace lab5_1
 	{
 		static void Main(string[] args)
 		{
+			/*
+			  Task:
+			  Enter the name of the football club. Count the number of characters in it.
+			  Display the name of the club on the console in a column (1 character per line).
+			*/
 			Console.WriteLine("Enter the name of the football club: ");
 			string teamName = Console.ReadLine();
       
