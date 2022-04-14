9f9d10dba00597a66c5100798348e2806f6afbf3
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-14 20:46:35 +0100

    Add files via upload

---
+++ Lab1/1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть своє призвiще:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Я студентка другого курсу - " + str + ". Це моя перша робота на С#");
+        }
+    }
+}
