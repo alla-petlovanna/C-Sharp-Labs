a412414c0fd0b9cd1dbdc11edcf7be2a44e12048
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-14 23:10:40 +0100

    Update Array_task_1.cs

--- Array_task_1.cs
+++ Array_task_1.cs
@@ -9,7 +9,8 @@ namespace lab4_1
 	{
 		static void Main()
 		{
-			/* Task:
+			/* 
+			   Task:
 			   Implement the function of finding all prime numbers in the range from 1 to 51. 
 			   Save the generated numbers in an array. Display the elements of the array on the console. 
 			   When implementing, use the Eratosthenes algorithm.
