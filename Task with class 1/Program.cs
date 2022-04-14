42fc2440001de3c258dff5c7521086bc555b6a25
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-14 23:05:18 +0100

    Update Program.cs

--- Task with class 1/Program.cs
+++ Task with class 1/Program.cs
@@ -11,6 +11,13 @@ namespace Lab7_1
 	{
 		static void Main(string[] args)
 		{
+			/* Task: 
+			   Develop a console application for working with the class Student with fields: name, age, course, rating. Make all fields of the class closed and access them due to properties. Implement a constructor with parameters.
+			   Create methods:
+			   EditStudent for editing student data. The method has 3 parameters for changing the values of the fields name, age, rating.
+			   StudentRating (double rating), which displays the student rating.
+			   GetRole (int course) - returns a string: bachelor or master depending on the course. 
+			*/
 			Student stud = new Student("Alla", 18, 2, 95);
 			string stRole = stud.GetRole(2);
 			Console.WriteLine("This student is a " + stRole);
